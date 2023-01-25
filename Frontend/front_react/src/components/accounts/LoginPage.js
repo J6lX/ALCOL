8053d3fddff801ca2d848c6c488548b8d6f57127
JeongYeonHwa <laboratory6889@gmail.com>
2023-01-25 17:44:07 +0900

    FEAT: 메인 페이지 구현 중

--- Frontend/front_react/src/components/accounts/LoginPage.js
+++ Frontend/front_react/src/components/accounts/LoginPage.js
@@ -13,68 +13,75 @@ function LoginPage() {
 
   return (
     <div className="fullmiddle">
-      <img className="bgimage" src="images/loginbg.jpg" alt="loginbg" />
-      <span className="overlay">
-        <div className="gradientsquare">
-          <div className="loginbackground">
-            <h1 className="whiteletter">로그인</h1>
-            <Form
-              name="normal_login"
-              className="login-form; middle"
-              initialValues={{ remember: true }}
-              onFinish={onFinish}>
-              <Form.Item
-                name="email"
-                rules={[{ required: true, message: "이메일을 입력해주세요." }]}
-                className="inputwidth">
-                <Input
-                  size="large"
-                  prefix={
-                    <MailOutlined className="site-form-item-icon" style={{ marginRight: "10px" }} />
-                  }
-                  placeholder="이메일"
-                  type="email"
-                />
-              </Form.Item>
-              <Form.Item
-                name="password"
-                rules={[{ required: true, message: "비밀번호를 입력해주세요." }]}
-                className="inputwidth">
-                <Input
-                  size="large"
-                  prefix={
-                    <LockOutlined className="site-form-item-icon" style={{ marginRight: "10px" }} />
-                  }
-                  type="password"
-                  placeholder="비밀번호"
-                />
-              </Form.Item>
-              <a className="login-form-forgot" style={{ color: "lightgray" }} href="/">
-                비밀번호를 잊으셨나요?
-              </a>
-              {/* <Form.Item>
+      <div className="bgimage">
+        <span className="overlay">
+          <div className="gradientsquare">
+            <div className="loginbackground">
+              <h1 className="whiteletter">로그인</h1>
+              <Form
+                name="normal_login"
+                className="login-form; middle"
+                initialValues={{ remember: true }}
+                onFinish={onFinish}>
+                <Form.Item
+                  name="email"
+                  rules={[{ required: true, message: "이메일을 입력해주세요." }]}
+                  className="inputwidth">
+                  <Input
+                    size="large"
+                    prefix={
+                      <MailOutlined
+                        className="site-form-item-icon"
+                        style={{ marginRight: "10px" }}
+                      />
+                    }
+                    placeholder="이메일"
+                    type="email"
+                  />
+                </Form.Item>
+                <Form.Item
+                  name="password"
+                  rules={[{ required: true, message: "비밀번호를 입력해주세요." }]}
+                  className="inputwidth">
+                  <Input
+                    size="large"
+                    prefix={
+                      <LockOutlined
+                        className="site-form-item-icon"
+                        style={{ marginRight: "10px" }}
+                      />
+                    }
+                    type="password"
+                    placeholder="비밀번호"
+                  />
+                </Form.Item>
+                <a className="login-form-forgot" style={{ color: "lightgray" }} href="/">
+                  비밀번호를 잊으셨나요?
+                </a>
+                {/* <Form.Item>
                 <Form.Item name="remember" valuePropName="checked" noStyle>
                   <Checkbox>Remember me</Checkbox>
                 </Form.Item>
               </Form.Item> */}
-              <br />
-              <Form.Item style={{ margin: "auto" }}>
-                <Button
-                  htmlType="submit"
-                  className="login-form-button"
-                  style={{ height: "40px", margin: "5px" }}>
-                  <b>로그인</b>
-                </Button>
-              </Form.Item>
-              <Form.Item>
-                <a style={{ color: "lightgray" }} href="/">
-                  회원가입
-                </a>
-              </Form.Item>
-            </Form>
+                <br />
+                <Form.Item style={{ margin: "auto" }}>
+                  <Button
+                    htmlType="submit"
+                    className="login-form-button"
+                    style={{ height: "40px", margin: "5px" }}>
+                    <b>로그인</b>
+                  </Button>
+                </Form.Item>
+                <Form.Item>
+                  <a style={{ color: "lightgray" }} href="/">
+                    회원가입
+                  </a>
+                </Form.Item>
+              </Form>
+            </div>
           </div>
-        </div>
-      </span>
+        </span>
+      </div>
     </div>
   );
 }
--- Frontend/front_react/src/components/home/HomePage.js
+++ Frontend/front_react/src/components/home/HomePage.js
@@ -1,7 +1,114 @@
 import React from "react";
+import video from "../../assets/homepage-main.mp4";
+import rankingStar from "../../assets/ranking_image.png";
+import { Button, Space, Col } from "antd";
+import "./HomePage.css";
+
+const MainPage = () => {
+  return (
+    <div className="fullmiddle">
+      <video loop autoPlay muted className="bgvideo">
+        <source src={video} type="video/mp4" />
+      </video>
+      <div className="mainh1">
+        <h1 style={{ fontSize: "56px" }}>AlCol에서</h1>
+        <h1 style={{ fontSize: "56px" }}>알고리즘 마스터로!</h1>
+      </div>
+      <div
+        className="battleStart"
+        style={{ height: "10%", border: "5px solid #FDE14B", borderRadius: "20px" }}>
+        <Button
+          style={{
+            width: "100%",
+            height: "100%",
+            border: "5px solid #440000",
+            borderRadius: "16px",
+          }}>
+          <b>
+            <p
+              className="NanumSquare"
+              style={{ fontSize: "32px", color: "#E51C29", fontWeight: "bold" }}>
+              Battle Start!
+            </p>
+          </b>
+        </Button>
+        <Button type="text" block style={{ marginTop: "5px" }}>
+          <p className="NanumSquare" style={{ fontSize: "16px", color: "white" }}>
+            배틀 방법이 궁금하신가요?
+          </p>
+        </Button>
+      </div>
+      <div className="gradientBox"></div>
+    </div>
+  );
+};
+
+const SpeedRanking = () => {
+  return (
+    <div>
+      <h1 style={{ color: "white" }}>스피드 랭킹 자리</h1>
+    </div>
+  );
+};
+
+const EfficiencyRanking = () => {
+  return (
+    <div>
+      <h1 style={{ color: "white" }}>최적화 랭킹 자리</h1>
+    </div>
+  );
+};
+
+const RankingPage = () => {
+  return (
+    <div className="fullmiddle" style={{ height: "800px", backgroundColor: "#14161A" }}>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <img src={rankingStar} alt="star" style={{ width: "15%" }} />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "center",
+            marginLeft: "20px",
+          }}>
+          <h1 className="NanumSquare" style={{ color: "white" }}>
+            Today's
+          </h1>
+          <h1 className="NanumSquare" style={{ fontSize: "48px", color: "white" }}>
+            <b>Ranking</b>
+          </h1>
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          textAlign: "center",
+          width: "80vw",
+          height: "70%",
+        }}>
+        <Col span={12}>
+          <SpeedRanking />
+        </Col>
+        <Col span={12}>
+          <EfficiencyRanking />
+        </Col>
+      </div>
+    </div>
+  );
+};
 
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <MainPage />
+      <RankingPage />
+    </div>
+  );
 };
 
 export default HomePage;
