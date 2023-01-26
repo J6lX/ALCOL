d098d47f33c02cd2190b1f7a9df02afd87669992
전동준 <jdznawa@naver.com>
2023-01-26 11:27:21 +0900

    DOCS: 랭킹 페이지 레이아웃 구성 중간저장

--- Frontend/front_react/src/components/mypage/Ranking.js
+++ Frontend/front_react/src/components/mypage/Ranking.js
@@ -1,7 +1,21 @@
 import { Button, Row, Col, Space } from "antd";
+import "./Ranking.css";
 
 function Ranking() {
-  return <h1>랭킹 페이지</h1>;
+  return (
+    <>
+      <Row justify="center">
+        <Col className="block" span={16}>
+          <h1>랭킹 페이지</h1>
+        </Col>
+      </Row>
+      <Row justify="center">
+        <Col className="block" offset={6} span={16}>
+          <h2>여기에 랭킹 정보 표시</h2>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
 export default Ranking;
