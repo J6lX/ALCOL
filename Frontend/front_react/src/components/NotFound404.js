431c8535faad7a325240defedc528480dad7458a
전동준 <jdznawa@naver.com>
2023-01-26 10:53:21 +0900

    헤더 링크 추가

--- Frontend/front_react/src/components/NotFound404.js
+++ Frontend/front_react/src/components/NotFound404.js
@@ -1,15 +1,22 @@
-import { Tooltip } from "antd";
+import { Row, Col } from "antd";
 
 function NotFound404() {
   return (
-    <Tooltip title="유효하지 않은 주소일 수 있습니다.">
-      <span
-        style={{
-          color: "white",
-        }}>
-        404 오류 : 페이지를 찾을 수 없습니다.
-      </span>
-    </Tooltip>
+    <Row
+      justify="center"
+      align="middle"
+      style={{
+        height: "200px",
+      }}>
+      <Col>
+        <span
+          style={{
+            color: "white",
+          }}>
+          404 오류 : 페이지를 찾을 수 없습니다.
+        </span>
+      </Col>
+    </Row>
   );
 }
 
