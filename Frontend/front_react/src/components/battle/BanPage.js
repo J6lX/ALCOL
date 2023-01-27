d2505d7efb508d8b71f390dd622a191f0d7598aa
Garden1298 <54656129+Garden1298@users.noreply.github.com>
2023-01-27 16:12:12 +0900

    FEAT - 밴 화면 배치

--- Frontend/front_react/src/components/battle/BanPage.js
+++ Frontend/front_react/src/components/battle/BanPage.js
@@ -5,10 +5,16 @@ import "./BanPage.css";
 function UserInfo() {
   return (
     <Row justify="end" className="battle_user_info_row">
-      <Col span={1} style={{ lineHeight: "50px" }} className="battle_user_info_contents">
+      <Col
+        span={1}
+        style={{ fontSize: "1vw", lineHeight: "50px" }}
+        className="battle_user_info_contents">
         멋진 티어
       </Col>
-      <Col span={1} style={{ lineHeight: "50px" }} className="battle_user_info_contents">
+      <Col
+        span={1}
+        style={{ fontSize: "1vw", lineHeight: "50px" }}
+        className="battle_user_info_contents">
         멋진 티어
       </Col>
       <Col
@@ -21,16 +27,59 @@ function UserInfo() {
   );
 }
 
+function PlayerInfo() {
+  return <div className="ban_player_info">Player1</div>;
+}
+
+function Top() {
+  return (
+    <Row>
+      <Col xs={12} sm={10} md={8} lg={6} xl={6}>
+        <PlayerInfo />
+      </Col>
+      <Col xs={12} sm={14} md={12} lg={12} xl={12} style={{ marginTop: "50px" }}>
+        <div className="ban_title">금지할 문제를 선택해주세요</div>
+        <div className="ban_info">
+          선택된 문제는 이번 게임에서 출제되지 않습니다. 만약 같은 문제를 금지했다면 남은 문제 중
+          랜덤하게 출제됩니다.
+        </div>
+      </Col>
+      <Col xs={0} sm={0} md={4} lg={6} xl={6}></Col>
+    </Row>
+  );
+}
+
+function Mid() {
+  return (
+    <Row justify="space-between" style={{ marginTop: "80px" }} className="ban_algo_contents">
+      <Col xs={0} sm={0} md={0} lg={4} xl={4}></Col>
+      <Col xs={7} sm={7} md={7} lg={4} xl={4} className="ban_algo_box"></Col>
+      <Col xs={7} sm={7} md={7} lg={4} xl={4} className="ban_algo_box"></Col>
+      <Col xs={7} sm={7} md={7} lg={4} xl={4} className="ban_algo_box"></Col>
+      <Col xs={0} sm={0} md={0} lg={4} xl={4}></Col>
+    </Row>
+  );
+}
+
+function Bottom() {
+  return (
+    <Row style={{ marginTop: "60px" }}>
+      <Col xs={0} sm={0} md={4} lg={7} xl={7}></Col>
+      <Col xs={12} sm={14} md={12} lg={10} xl={10} style={{ marginTop: "100px" }}></Col>
+      <Col xs={12} sm={10} md={8} lg={7} xl={7}>
+        <PlayerInfo />
+      </Col>
+    </Row>
+  );
+}
+
 function App() {
   return (
     <div className="matching_background">
       <UserInfo />
-      <div className="ban_title">금지할 문제를 선택해주세요</div>
-      <div className="ban_info">
-        선택된 문제는 이번 게임에서 출제되지 않습니다. 만약 같은 문제를 금지했다면 남은 문제 중
-        랜덤하게 출제됩니다.
-      </div>
-      <div></div>
+      <Top />
+      <Mid />
+      <Bottom />
       <div className="matchingButton">취소</div>
     </div>
   );
