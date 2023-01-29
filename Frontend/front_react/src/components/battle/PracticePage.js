450806067e12938af84175b91d4e7a52287a3ed8
전동준 <jdznawa@naver.com>
2023-01-29 13:39:26 +0900

    FEAT: 문제 목록 표시 구현

--- Frontend/front_react/src/components/battle/PracticePage.js
+++ Frontend/front_react/src/components/battle/PracticePage.js
@@ -1,108 +1,100 @@
 import { Button, Row, Col, Pagination, Input } from "antd";
 import "./PracticePage.css";
 import practiceHeader from "../../assets/practice_header.png";
+// MaterialUI(MUI) 사용: 프레임워크 다운로드 필요
+// npm install @mui/x-data-grid 입력하여 다운로드
+import { DataGrid } from "@mui/x-data-grid";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+// 문제 목록 다크 모드 적용
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+// 연습 문제 구분 설명
+const problemLabel = [
+  { field: "id", headerName: "문제 번호", width: 90, height: 200 },
+  { field: "name", headerName: "문제 이름", width: 90, height: 200 },
+  { field: "type", headerName: "문제 유형", width: 90, height: 200 },
+  { field: "tier", headerName: "난이도", width: 90, height: 200 },
+];
+
+// 연습 문제 데이터
+const problemData = [
+  { id: 1, name: "문제1", type: "DFS", tier: "Gold" },
+  { id: 2, name: "문제2", type: "BFS", tier: "Gold" },
+  { id: 3, name: "문제3", type: "Greedy", tier: "Gold" },
+  { id: 4, name: "문제4", type: "DFS", tier: "Gold" },
+  { id: 5, name: "문제5", type: "DP", tier: "Gold" },
+];
 
 function Ranking() {
   return (
-    <>
-      <div>
-        {/* 페이지 제목(이미지 위에 띄우기) */}
-        <Row justify="center">
-          <Col align="middle" span={16} className="title">
-            <h1>연습 문제 풀기</h1>
-          </Col>
-        </Row>
-        <img
-          src={practiceHeader}
-          alt="rankingHeader"
-          className="headerImg"
-          style={{
-            maxWidth: "100%",
-            maxHeight: "100%",
-          }}></img>
-        <Row justify="space-around" className="bodyblock">
-          <Col span={16}>
-            {/* 검색 상자 */}
-            <Row justify="end">
-              <Col xs={8} lg={5}>
-                <Input
-                  placeholder="유형 이름 검색"
-                  allowClear
-                  size="middle"
-                  style={{
-                    margin: "5px",
-                  }}
-                />
-              </Col>
-              <Col
+    <div>
+      {/* 페이지 제목(이미지 위에 띄우기) */}
+      <Row justify="center">
+        <Col align="middle" span={16} className="title">
+          <h1>연습 문제 풀기</h1>
+        </Col>
+      </Row>
+      <img
+        src={practiceHeader}
+        alt="rankingHeader"
+        className="headerImg"
+        style={{
+          maxWidth: "100%",
+          maxHeight: "100%",
+        }}></img>
+      <Row justify="space-around" className="bodyblock">
+        <Col span={16}>
+          {/* 검색 상자 */}
+          <Row justify="end">
+            <Col xs={8} lg={5}>
+              <Input
+                placeholder="유형 이름 검색"
+                allowClear
+                size="middle"
                 style={{
-                  padding: "5px",
-                }}>
-                <Button>검색</Button>
-              </Col>
-            </Row>
+                  margin: "5px",
+                }}
+              />
+            </Col>
+            <Col
+              style={{
+                padding: "5px",
+              }}>
+              <Button>검색</Button>
+            </Col>
+          </Row>
 
-            {/* 랭킹 표시 블록 */}
-            <Row>
-              <Col span={24}>
-                <Row className="block" justify="center" align="center">
-                  <Col span={24}>
-                    {/* 상단 설명 바 목록 */}
-                    <Row className="select">
-                      <Col span={4}>
-                        <h3>문제 번호</h3>
-                      </Col>
-                      <Col span={5}>
-                        <h3>문제 이름</h3>
-                      </Col>
-                      <Col span={9}>
-                        <h3>문제 유형</h3>
-                      </Col>
-                      <Col span={6}>
-                        <h3>문제 난이도</h3>
-                      </Col>
-                    </Row>
-                    <hr></hr>
+          {/* 연습 문제 목록 블록 */}
+          <Row className="block" justify="center" align="center">
+            <Col className="problems" span={24}>
+              <ThemeProvider theme={darkTheme}>
+                <DataGrid rows={problemData} columns={problemLabel} />
+              </ThemeProvider>
+            </Col>
+          </Row>
 
-                    {/* 문제 목록 표시 */}
-
-                    <Row justify="center">
-                      <Col span={4}>
-                        <h3>ProblemNo</h3>
-                      </Col>
-                      <Col span={5}>
-                        <h3>ProblemName</h3>
-                      </Col>
-                      <Col span={9}>
-                        <h3>ProblemType</h3>
-                      </Col>
-                      <Col span={6}>
-                        <h3>ProblemDifficulty</h3>
-                      </Col>
-                    </Row>
-                  </Col>
-                </Row>
-
-                {/* 페이지네이션 표시 */}
-                <Row justify="center">
-                  <Col align="center">
-                    <Pagination
-                      defaultCurrent={1}
-                      total={50}
-                      responsive="true"
-                      theme="dark"
-                      style={{
-                        padding: "10px",
-                      }}
-                    />
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </div>
-    </>
+          {/* 페이지네이션 표시 */}
+          <Row justify="center">
+            <Col align="center">
+              <Pagination
+                defaultCurrent={1}
+                total={50}
+                responsive="true"
+                theme="dark"
+                style={{
+                  padding: "10px",
+                }}
+              />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
   );
 }
 
