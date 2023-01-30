4f1f6a60a98af758c871c415cb4ee4ac7ecfd265
“김주성” <dlrtls12345@naver.com>
2023-01-30 16:37:19 +0900

    FIX: hotfix

--- Backend/alcol/log-service/src/main/java/com/alcol/logservice/controller/LogController.java
+++ Backend/alcol/log-service/src/main/java/com/alcol/logservice/controller/LogController.java
@@ -16,7 +16,7 @@ public class LogController
     public List<String> getLog(@RequestParam(value="param") String plusData)
     {
         log.info("user-service -> log-service 호출 완료!!");
-        log.info("log-service 로 전달된 plusData", plusData);
+        log.info("log-service 로 전달된 plusData : " + plusData);
         List<String> logs = new ArrayList<>();
         for (int i = 1; i <= 5; i++)
         {
