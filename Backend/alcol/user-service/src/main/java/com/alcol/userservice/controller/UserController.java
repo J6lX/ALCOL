2f8c4a50bbfdb526f6fe8df4930b60faefacdf6b
“김주성” <dlrtls12345@naver.com>
2023-01-30 16:21:15 +0900

    FEAT: restTemplate Test 완료

--- Backend/alcol/user-service/src/main/java/com/alcol/userservice/controller/UserController.java
+++ Backend/alcol/user-service/src/main/java/com/alcol/userservice/controller/UserController.java
@@ -8,11 +8,15 @@ import org.apache.http.HttpHeaders;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.bind.DatatypeConverter;
+import java.util.List;
 
 
 @RestController
@@ -22,11 +26,17 @@ public class UserController
 {
     private final UserService userService;
     private final Environment env;
+    private final RestTemplate restTemplate;
 
-    public UserController(UserService userService, Environment env)
+    public UserController(
+            UserService userService,
+            Environment env,
+            RestTemplate restTemplate
+    )
     {
         this.userService = userService;
         this.env = env;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping("/hello")
@@ -62,4 +72,20 @@ public class UserController
         response.addHeader("new-access-token", newAccessToken);
         return ResponseEntity.status(HttpStatus.CREATED).body("새로운 access token 이 발급되었습니다.");
     }
+
+    // RestTemplate 을 통한 서비스 간 호출 테스트
+    @GetMapping("/restTemplate")
+    public ResponseEntity<List> restTemplateTestToLogService()
+    {
+        MultiValueMap<String, String> bodyData = new LinkedMultiValueMap<>();
+        bodyData.add("param", "this is plus data");
+        String url = "http://localhost:9005/log-service/getLog";
+        ResponseEntity<List> logs = restTemplate.postForEntity(
+                url,
+                bodyData,
+                List.class
+        );
+        return logs;
+    }
+
 }
