f27c7f6b32954bbcd4f03326d1938d2c1a87e679
PARKSEOYOUNG <gksdidtk123@gmail.com>
2023-01-31 09:24:22 +0900

    CHORE: add Entity

---
+++ Backend/alcol/rank-service/src/main/java/com/alcol/rankservice/Entity/Rank.java
@@ -0,0 +1,13 @@
+package com.alcol.rankservice.Entity;
+
+import com.alcol.rankservice.dto.RecordDto;
+
+public class Rank
+{
+    private String profile_pic;
+    private String nickname;
+    private int level;
+    private String tier;
+    private int MMR;
+    private RecordDto record;
+}
