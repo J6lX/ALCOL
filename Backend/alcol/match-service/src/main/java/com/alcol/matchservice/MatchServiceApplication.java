1d4050402ed318e79db7d085da92e4f94983f9da
DHelloper <wjdehgud5769@naver.com>
2023-02-03 17:55:48 +0900

    FEAT: matching 기능 구현중

--- Backend/alcol/match-service/src/main/java/com/alcol/matchservice/MatchServiceApplication.java
+++ Backend/alcol/match-service/src/main/java/com/alcol/matchservice/MatchServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
@@ -18,5 +19,10 @@ public class MatchServiceApplication {
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
     }
 }
