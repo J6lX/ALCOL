89e9000673e6dc1a8eccba5a8dbf15ebac8e3f65
전동준 <jdznawa@naver.com>
2023-02-03 10:56:53 +0900

    FIX: 푸터가 표시되지 않는 현상 해결

--- Frontend/front_react/src/components/mypage/Mypage.js
+++ Frontend/front_react/src/components/mypage/Mypage.js
@@ -1,4 +1,3 @@
-import Container from "react-bootstrap/Container";
 import { Row, Col, Table } from "antd";
 import "./Mypage.css";
 import settingIcon from "../../assets/setting.png";
@@ -161,168 +160,166 @@ function Mypage() {
   const profile = userData[userInfo.username];
 
   return (
-    <div>
+    <div
+      className="pageBody"
+      style={{
+        backgroundColor: "#16171B",
+        padding: "30px",
+        minHeight: "150vh",
+      }}>
       {profile ? (
-        <div
-          className="pageBody"
-          style={{
-            backgroundColor: "#16171B",
-            padding: "30px",
-          }}>
-          {/* Container 태그 내 4개 블록으로 분리 */}
-          <Container>
-            {/* 사용자 프로필 블록 */}
-            <Row
-              justify="center"
+        <div>
+          {/* 사용자 프로필 블록 */}
+          <Row
+            justify="center"
+            style={{
+              padding: "5px",
+            }}>
+            <Col xs={16} md={6} lg={4} className="text block">
+              {/* 설정 아이콘 표시 블록 */}
+              <Row>
+                <Col md={{ offset: 21 }}>
+                  <Link to="/modify">
+                    <img src={settingIcon} alt="settings" className="settingIcon" />
+                  </Link>
+                </Col>
+              </Row>
+
+              {/* 개인 정보 표시 블록 */}
+              <Row justify="center">
+                <Col>
+                  {/* 이미지를 정상적으로 불러올 수 없는 경우 대체 이미지가 납작하게 표시되는 현상 발생 중 */}
+                  <img src={tempImg} alt="프사" className="userImg" />
+                  <h1>{profile.name}</h1>
+                </Col>
+              </Row>
+            </Col>
+
+            {/* 스트릭 및 티어 정보 표시 블록*/}
+            <Col xs={16} lg={18} className="textHighlight block">
+              <Row>
+                {/* 티어 정보 표시 블록 */}
+                <Col span={24} justify="center" align="middle">
+                  <Row justify="center">
+                    {/* 스피드전 티어 뱃지 */}
+                    <Col span={6} justify="center">
+                      <img src={tempImg} alt="프사" className="userImg"></img>
+                    </Col>
+                    {/* 최적화전 티어 뱃지 */}
+                    <Col span={6} justify="center">
+                      <img src={tempImg} alt="프사" className="userImg"></img>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+
+          {/* 친구 정보, 지난 시즌, 전적 표시 */}
+          <Row
+            justify="center"
+            style={{
+              padding: "5px",
+            }}>
+            <Col
+              xs={16}
+              md={6}
+              lg={4}
+              align="middle"
               style={{
-                padding: "5px",
+                margin: "10px",
               }}>
-              <Col xs={16} md={6} lg={4} className="text block">
-                {/* 설정 아이콘 표시 블록 */}
-                <Row>
-                  <Col md={{ offset: 21 }}>
-                    <Link to="/modify">
-                      <img src={settingIcon} alt="settings" className="settingIcon" />
-                    </Link>
-                  </Col>
-                </Row>
-
-                {/* 개인 정보 표시 블록 */}
-                <Row justify="center">
-                  <Col>
-                    {/* 이미지를 정상적으로 불러올 수 없는 경우 대체 이미지가 납작하게 표시되는 현상 발생 중 */}
-                    <img src={tempImg} alt="프사" className="userImg" />
-                    <h1>{profile.name}</h1>
-                  </Col>
-                </Row>
-              </Col>
-
-              {/* 스트릭 및 티어 정보 표시 블록*/}
-              <Col xs={16} lg={18} className="textHighlight block">
-                <Row>
-                  {/* 티어 정보 표시 블록 */}
-                  <Col span={24} justify="center" align="middle">
-                    <Row justify="center">
-                      {/* 스피드전 티어 뱃지 */}
-                      <Col span={6} justify="center">
-                        <img src={tempImg} alt="프사" className="userImg"></img>
-                      </Col>
-                      {/* 최적화전 티어 뱃지 */}
-                      <Col span={6} justify="center">
-                        <img src={tempImg} alt="프사" className="userImg"></img>
-                      </Col>
-                    </Row>
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-
-            {/* 친구 정보, 지난 시즌, 전적 표시 */}
-            <Row
-              justify="center"
-              style={{
-                padding: "5px",
-              }}>
-              <Col
-                xs={16}
-                md={6}
-                lg={4}
-                align="middle"
+              <Row
+                justify="center"
                 style={{
-                  margin: "10px",
+                  paddingBottom: "15px",
                 }}>
-                <Row
-                  justify="center"
-                  style={{
-                    paddingBottom: "15px",
-                  }}>
-                  {/* 친구 정보 표시 */}
-                  <Col xs={24} className="textHighlight miniBlock">
-                    <p>친구 정보</p>
-                    <hr />
-                    <p>친구 1</p>
-                  </Col>
-                </Row>
+                {/* 친구 정보 표시 */}
+                <Col xs={24} className="textHighlight miniBlock">
+                  <p>친구 정보</p>
+                  <hr />
+                  <p>친구 1</p>
+                </Col>
+              </Row>
 
-                <Row>
-                  {/* 지난 시즌 기록 보기 */}
-                  <Col xs={24} className="miniBlock">
-                    <p className="textHighlight">지난 시즌 기록</p>
-                    <hr />
-                    <p className="textHighlight">시즌 기록 없음</p>
-                    <hr />
-                    {/* 지난 시즌 이력 조회 링크 */}
-                    <Link to={`/season/${userInfo.username}`}>
-                      <p className="textHighlight">지난 시즌 조회</p>
-                    </Link>
-                  </Col>
-                </Row>
-              </Col>
+              <Row>
+                {/* 지난 시즌 기록 보기 */}
+                <Col xs={24} className="miniBlock">
+                  <p className="textHighlight">지난 시즌 기록</p>
+                  <hr />
+                  <p className="textHighlight">시즌 기록 없음</p>
+                  <hr />
+                  {/* 지난 시즌 이력 조회 링크 */}
+                  <Link to={`/season/${userInfo.username}`}>
+                    <p className="textHighlight">지난 시즌 조회</p>
+                  </Link>
+                </Col>
+              </Row>
+            </Col>
 
-              {/* 전적 정보 표시 블록 */}
-              <Col xs={16} lg={18} className="textHighlight block">
-                {/* 필터 블록(모두/스피드전/효율성전 선택 버튼) */}
-                <Row justify="space-around" className="modeFilter">
-                  <Col sm={4} lg={4}>
-                    <h4>모두</h4>
-                  </Col>
-                  <Col sm={4} lg={4}>
-                    <h4>스피드전</h4>
-                  </Col>
-                  <Col sm={4} lg={4}>
-                    <h4>최적화전</h4>
-                  </Col>
-                </Row>
+            {/* 전적 정보 표시 블록 */}
+            <Col xs={16} lg={18} className="textHighlight block">
+              {/* 필터 블록(모두/스피드전/효율성전 선택 버튼) */}
+              <Row justify="space-around" className="modeFilter">
+                <Col sm={4} lg={4}>
+                  <h4>모두</h4>
+                </Col>
+                <Col sm={4} lg={4}>
+                  <h4>스피드전</h4>
+                </Col>
+                <Col sm={4} lg={4}>
+                  <h4>최적화전</h4>
+                </Col>
+              </Row>
 
-                {/* 도넛 그래프 블록(최근 20전) */}
-                <Row justify="center" style={{ height: "270px" }}>
-                  <ResponsivePie
-                    data={recentRecord}
-                    margin={{ top: 40, right: 80, bottom: 40, left: 80 }}
-                    innerRadius={0.5}
-                    padAngle={0.7}
-                    cornerRadius={3}
-                    activeOuterRadiusOffset={8}
-                    colors={["#5cfdfd", "#FDE14B"]}
-                    colorBy="index"
-                    borderWidth={1}
-                    borderColor={{ theme: "background" }}
-                    enableArcLabels={false}
-                    enableArcLinkLabels={false}
-                    arcLabelsSkipAngle={10}
-                    isInteractive={false}
-                    layers={["arcs", "arcLabels", "arcLinkLabels", "legends", CenteredMetric]}
-                    fill={[
-                      {
-                        match: {
-                          id: "win",
-                        },
+              {/* 도넛 그래프 블록(최근 20전) */}
+              <Row justify="center" style={{ height: "270px" }}>
+                <ResponsivePie
+                  data={recentRecord}
+                  margin={{ top: 40, right: 80, bottom: 40, left: 80 }}
+                  innerRadius={0.5}
+                  padAngle={0.7}
+                  cornerRadius={3}
+                  activeOuterRadiusOffset={8}
+                  colors={["#5cfdfd", "#FDE14B"]}
+                  colorBy="index"
+                  borderWidth={1}
+                  borderColor={{ theme: "background" }}
+                  enableArcLabels={false}
+                  enableArcLinkLabels={false}
+                  arcLabelsSkipAngle={10}
+                  isInteractive={false}
+                  layers={["arcs", "arcLabels", "arcLinkLabels", "legends", CenteredMetric]}
+                  fill={[
+                    {
+                      match: {
+                        id: "win",
                       },
-                      {
-                        match: {
-                          id: "lose",
-                        },
+                    },
+                    {
+                      match: {
+                        id: "lose",
                       },
-                    ]}
-                    legends={[]}
-                  />
-                </Row>
-                {/* 전적 표시 블록 */}
-                <Row>
-                  <Col span={24}>
-                    <Table theme="dark" columns={matchCol} dataSource={matchData} />
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-          </Container>
+                    },
+                  ]}
+                  legends={[]}
+                />
+              </Row>
+              {/* 전적 표시 블록 */}
+              <Row>
+                <Col span={24}>
+                  <Table theme="dark" columns={matchCol} dataSource={matchData} />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
         </div>
       ) : (
         <Row
           justify="center"
           align="middle"
           style={{
-            height: "500px",
+            height: "400px",
           }}>
           <Col align="middle">
             <span
