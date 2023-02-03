544332d5499ee6c25fb793fc120b811f111ee20e
Garden1298 <54656129+Garden1298@users.noreply.github.com>
2023-02-03 16:32:30 +0900

    FEAT : 결과 페이지 결과 타이틀 결과 데이터에 따라 변경

--- Frontend/front_react/src/components/battle/ResultPage.js
+++ Frontend/front_react/src/components/battle/ResultPage.js
@@ -1,18 +1,31 @@
 import React from "react";
 import "./ResultPage.css";
-import img_victory from "../../assets/result_victory.png";
+import img_victory from "../../assets/result_victory.gif";
+import img_defeat from "../../assets/result_defeat.gif";
 import { Col, Row } from "antd";
 
 function App() {
+  // eslint-disable-next-line
   const [result, setResult] = React.useState({
-    battle_result: "28:35",
+    battle_result: "win",
     get_exp: "300",
     get_mmr: "25",
   });
 
+  const printResult = () => {
+    var img = "";
+    if (result.battle_result === "win") {
+      img = <img src={img_victory} alt="result" className="result_title" />;
+    } else {
+      img = <img src={img_defeat} alt="result" className="result_title" />;
+    }
+    return img;
+  };
+
   return (
     <div className="result_background">
-      <img src={img_victory} alt="result" className="result_title" />
+      {/* <img src={img_victory} alt="result" className="result_title" /> */}
+      {printResult()}
       <div className="result_box">
         <div className="result_content">
           <Row>
