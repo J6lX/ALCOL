5be330db993613826173fa4a6fb64ffd138dc77d
“김주성” <dlrtls12345@naver.com>
2023-02-07 02:49:07 +0900

    FEAT: problem-service mypage 구현중

---
+++ Backend/alcol/problem-service/src/main/java/com/alcol/problemservice/controller/ProblemController.java
@@ -0,0 +1,12 @@
+package com.alcol.problemservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/problem-service")
+@Slf4j
+public class ProblemController
+{
+}
---
+++ Backend/alcol/problem-service/src/main/java/com/alcol/problemservice/service/ProblemService.java
@@ -0,0 +1,5 @@
+package com.alcol.problemservice.service;
+
+public interface ProblemService
+{
+}
