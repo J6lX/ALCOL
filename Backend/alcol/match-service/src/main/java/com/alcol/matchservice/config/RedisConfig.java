48fe107c1b45d6b4d59cce6df45479964ce7d4ff
DHelloper <wjdehgud5769@naver.com>
2023-02-08 13:20:39 +0900

    FEAT: 매칭 구현 완료

--- Backend/alcol/match-service/src/main/java/com/alcol/matchservice/config/RedisConfig.java
+++ Backend/alcol/match-service/src/main/java/com/alcol/matchservice/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -19,10 +20,16 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int redisPort;
 
+    @Value("${spring.redis.password}")
+    private String redisPwd;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisHost, redisPort);
-        return lettuceConnectionFactory;
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(redisPort);
+        redisStandaloneConfiguration.setPassword(redisPwd);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
