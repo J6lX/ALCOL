b619a8a012a0c0692ccf1f5957c163b96f26e527
Garden1298 <54656129+Garden1298@users.noreply.github.com>
2023-02-08 13:02:26 +0900

    FEAT : 회원가입 경고창 작업중

--- Frontend/front_react/src/components/AppHeader.js
+++ Frontend/front_react/src/components/AppHeader.js
@@ -14,8 +14,8 @@ function LoginTag(props) {
   // isLoggedIn === 로그인 상태 체크(임시 변수)
   const isLoggedIn = true;
   // 로그인 페이지에서 세션에 인증 정보를 저장한 후sessionStorage 또는 localStorage에서 사용자 인증 여부 가져오기
-  console.log(sessionStorage);
-  console.log(localStorage);
+  // console.log(sessionStorage);
+  // console.log(localStorage);
 
   // 로그인 한 경우(isLoggedin === true인 경우) 회원 정보 표시
   if (isLoggedIn) {
@@ -137,7 +137,7 @@ function HeaderData() {
   const browser = ResizedComponent();
 
   // 큰 화면에서 표시하는 데이터
-  console.log(browser.width);
+  // console.log(browser.width);
   if (browser.width >= 640) {
     return (
       <Header
