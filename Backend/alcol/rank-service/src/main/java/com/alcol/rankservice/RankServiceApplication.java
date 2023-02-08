80f3e8e1f8100ef2691f90627e2f0b2c513c1e9b
박서영 <gksdidtk123@gmail.com>
2023-02-08 15:54:58 +0900

    STYLE: code cleanup

--- Backend/alcol/rank-service/src/main/java/com/alcol/rankservice/RankServiceApplication.java
+++ Backend/alcol/rank-service/src/main/java/com/alcol/rankservice/RankServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class RankServiceApplication {
 
     public static void main(String[] args) {
