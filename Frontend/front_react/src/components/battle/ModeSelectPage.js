f59790220cdb72140611a7ede5171906056b2d01
Garden1298 <54656129+Garden1298@users.noreply.github.com>
2023-02-09 14:54:20 +0900

    FEAT : 안쓰는 코드 삭제

--- Frontend/front_react/src/components/battle/ModeSelectPage.js
+++ Frontend/front_react/src/components/battle/ModeSelectPage.js
@@ -273,41 +273,6 @@ function App() {
     }
   }, [language, history]);
 
-  //-------- 흔들리는 효과
-  const docStyle = document.documentElement.style;
-  const aElem = document.querySelector("a");
-  const boundingClientRect = aElem.getBoundingClientRect();
-
-  aElem.onmousemove = function (e) {
-    const x = e.clientX - boundingClientRect.left;
-    const y = e.clientY - boundingClientRect.top;
-
-    const xc = boundingClientRect.width / 2;
-    const yc = boundingClientRect.height / 2;
-
-    const dx = x - xc;
-    const dy = y - yc;
-
-    docStyle.setProperty("--rx", `${dy / -1}deg`);
-    docStyle.setProperty("--ry", `${dx / 10}deg`);
-  };
-
-  aElem.onmouseleave = function (e) {
-    docStyle.setProperty("--ty", "0");
-    docStyle.setProperty("--rx", "0");
-    docStyle.setProperty("--ry", "0");
-  };
-
-  aElem.onmousedown = function (e) {
-    docStyle.setProperty("--tz", "-25px");
-  };
-
-  document.body.onmouseup = function (e) {
-    docStyle.setProperty("--tz", "-12px");
-  };
-
-  //---- 흔들 효과 끝
-
   return (
     <div className="battle_background animate__animated animate__fadeIn">
       <UserInfo setMode={setMode} setLanguage={setLanguage} />
