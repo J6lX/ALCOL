5d4effb538805718ef5d14f5e3f3e4a8b7fa39a9
“김주성” <dlrtls12345@naver.com>
2023-02-09 14:13:25 +0900

    FIX: cors

--- Backend/alcol/apigateway-service/src/main/java/com/alcol/apigatewayservice/filter/PreFlightCorsConfiguration.java
+++ Backend/alcol/apigateway-service/src/main/java/com/alcol/apigatewayservice/filter/PreFlightCorsConfiguration.java
@@ -16,7 +16,7 @@ import reactor.core.publisher.Mono;
 @Configuration
 public class PreFlightCorsConfiguration
 {
-    private static final String ALLOWED_ORIGIN = "http://i8b303.p.ssafy.io/**";
+    private static final String ALLOWED_ORIGIN = "http://i8b303.p.ssafy.io";
     private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type";
     private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS";
     private static final String ALLOWED_CREDENTIALS = "true";
