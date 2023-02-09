3684ad5db09f30a0976a14b8be8104844d995483
PARKSEOYOUNG <gksdidtk123@gmail.com>
2023-02-09 20:40:50 +0900

    FEAT: mmr이 있는지 확인하던 코드 -> 유저의 승패 기록이 있는지 확인하는 코드

--- Backend/alcol/rank-service/src/main/java/com/alcol/rankservice/service/RankService.java
+++ Backend/alcol/rank-service/src/main/java/com/alcol/rankservice/service/RankService.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 public interface RankService
 {
-    public int confirmUserRanking(String userId, String mode);
     public RankDto.Ranking responseUserInfo(String userId, String battleMode);
     public RankDto.UserData getUserData(String userId);
     public RankDto.WinLoseCount getWinLoseCount(String userId, String battleMode);
