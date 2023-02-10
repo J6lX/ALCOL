3b6c9024fc02bea0bf4f1087d8f55391172e560b
JeongYeonHwa <laboratory6889@gmail.com>
2023-02-10 13:11:22 +0900

    CHORE: 작은 수정

--- Frontend/front_react/src/components/battle/SelectedProblemPage.js
+++ Frontend/front_react/src/components/battle/SelectedProblemPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Row, Col, Badge } from "antd";
 import versus from "../../assets/versus.png";
 // import $ from "jquery";
