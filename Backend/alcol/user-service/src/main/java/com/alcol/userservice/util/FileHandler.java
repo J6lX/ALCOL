c10e5dbdf3adc5fc725d620190a3574eaedb2d57
“김주성” <dlrtls12345@naver.com>
2023-02-10 13:22:44 +0900

    FEAT: 로커ë ì»컬에서는 테ì트 완료

--- Backend/alcol/user-service/src/main/java/com/alcol/userservice/util/FileHandler.java
+++ Backend/alcol/user-service/src/main/java/com/alcol/userservice/util/FileHandler.java
@@ -2,6 +2,7 @@ package com.alcol.userservice.util;
 
 import com.alcol.userservice.entity.UserEntity;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,20 +16,34 @@ import java.util.Date;
 @Slf4j
 public class FileHandler
 {
-    public boolean parseFileInfo(MultipartFile multipartFile, UserEntity userEntity)
+    private final ResourceLoader resourceLoader;
+
+    public FileHandler(ResourceLoader resourceLoader)
     {
+        this.resourceLoader = resourceLoader;
+    }
+
+    public boolean parseFileInfo(MultipartFile multipartFile, UserEntity userEntity) throws IOException {
         // 파일 이름을 업로드 한 날짜로 바꾸어서 저장
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
-        String currentDate = simpleDateFormat.format(new Date());
+//        String currentDate = simpleDateFormat.format(new Date());
 
         // 프로젝트 폴더에 저장하기 위해 절대경로를 설정
-        String absolutePath = new File("").getAbsolutePath();
+//        String absolutePath = new File("").getAbsolutePath();
 
 //        log.info("absolute path : " + absolutePath);
 
+        String relativeFolder = resourceLoader.getResource("classpath:/static").getURI().getPath();
+//                + "resources"
+//                + File.separator + "static"
+//                + File.separator + "images"
+//                + File.separator + currentDate;
+
+        log.info("relativeFolder : " + relativeFolder);
+
         // 경로를 지정하고 그곳에 저장
-        String path = "images/" + currentDate;
-        File file = new File(path);
+//        String path = "images/" + currentDate;
+        File file = new File(relativeFolder);
 
         // 저장할 위치의 디렉토리가 존재하지 않을 경우
         if (!file.exists())
@@ -66,7 +81,7 @@ public class FileHandler
         String new_file_name = System.nanoTime() + originalFileExtension;
 
         // 저장된 파일로 변경하여 이를 보여주기 위함
-        file = new File(absolutePath + path + "/" + new_file_name);
+        file = new File(relativeFolder + File.separator + new_file_name);
 
         // 파일을 저장
         try {
