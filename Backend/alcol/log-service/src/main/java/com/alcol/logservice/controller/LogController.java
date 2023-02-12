09d92de331090efba5db0a77734f056bb4bf8cfd
“김주성” <dlrtls12345@naver.com>
2023-02-12 19:08:45 +0900

    fix: cors

--- Backend/alcol/log-service/src/main/java/com/alcol/logservice/controller/LogController.java
+++ Backend/alcol/log-service/src/main/java/com/alcol/logservice/controller/LogController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/log-service")
 @Slf4j
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class LogController
 {
     private final LogService logService;
